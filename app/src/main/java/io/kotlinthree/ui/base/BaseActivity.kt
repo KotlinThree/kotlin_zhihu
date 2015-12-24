f749a5720556e01333ac25201f2e07f4deded5ce
huachangjian <huachangjian@hujiang.com>
2015-12-24 13:41:52 +0800

    add umeng

--- app/src/main/java/io/kotlinthree/ui/base/BaseActivity.kt
+++ app/src/main/java/io/kotlinthree/ui/base/BaseActivity.kt
@@ -3,6 +3,7 @@ package io.kotlinthree.ui.base
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.umeng.analytics.MobclickAgent
 
 /**
  * Created by jameson on 12/18/15.
@@ -13,5 +14,15 @@ public open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this;
+    }
+
+    override fun onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+
+    override fun onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
     }
 }
